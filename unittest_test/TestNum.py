dbf15bbf94da7603b7946746e387100b03939a3e
caijiajun <1240055602@qq.com>
2020-05-06 16:22:11 +0800

    pytest相关基础操作模拟练习

--- unittest_test/TestNum.py
+++ unittest_test/TestNum.py
@@ -1,5 +1,6 @@
 # 1、导入unittest包
 import unittest
+import pytest
 
 
 # 2、创建类，继承unittest.TestCase
@@ -18,21 +19,22 @@ class Test(unittest.TestCase):
     @unittest.skipUnless(2 < 1, "不成立跳过测试用例")
     def test_num_02(self):
         print("num02")
-        # self.assertEqual(1, 2)
+        # self.assertEqual(1,2)
         # self.assertNotEqual(1, 2)
-        # self.assertTrue(1 > 2, "1>2是错误的")  # 失败
-        # self.assertFalse(1 > 2)  # 成功
+        # self.assertTrue(1>2,"1>2是错误的") #失败
+        # self.assertFalse(1>2)#成功
         # self.assertIn("h","hello")#成功
+
         self.assertNotIn("h", "hello")  # 失败
 
     # @unittest.skipIf(2>1,"因为2>1,不想执行了")
     @unittest.skipIf(2 < 1, "因为2<1,不想执行了")
     def test_num_01(self):
         print("num01")
-        # self.assertEqual(1, 1)
-        # self.assertNotEqual(1, 1, "内容相等了")
-        # self.assertTrue(True)  # 成功
-        # self.assertFalse(True)  # 失败
+        # self.assertEqual(1,1)
+        # self.assertNotEqual(1, 1,"内容相等了")
+        # self.assertTrue(True)   #成功
+        # self.assertFalse(True) #失败
         # self.assertIn("a","hello","内容不存在")#失败
 
         self.assertNotIn("a", "hello", "内容不存在")
@@ -43,3 +45,7 @@ class Test(unittest.TestCase):
     @classmethod
     def tearDownClass(cls):
         print("测试用例执行后只执行一次")
+
+
+if __name__ == '__main__':
+    pytest.main(["-s", "TestNum.py"])
